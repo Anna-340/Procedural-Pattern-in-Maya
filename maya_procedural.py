f021d4aebc241593a6792f24c6839674ad88c862
Annaliese <Annaliesepalm3@gmail.com>
2025-10-15 21:17:45 -0500

    adding psudo code

---
+++ maya_procedural.py
@@ -0,0 +1,5 @@
+#Three simple challenges or one complex one 
+#Custom Maya shelf button and icon 
+#Number of intuative/paramters for asset generator (5 to 10) arguments
+#Challenge features
+#User Interface
