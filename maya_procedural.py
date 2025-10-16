ceb22075b19a24fbb30a6044294c60d09a9c6edf
Annaliese <Annaliesepalm3@gmail.com>
2025-10-15 22:26:14 -0500

    pesudo code for planning the program

--- maya_procedural.py
+++ maya_procedural.py
@@ -1,5 +1,12 @@
 #Three simple challenges or one complex one 
-#Custom Maya shelf button and icon 
+#-
+
+#Custom Maya shelf button and icon & user interface
+
 #Number of intuative/paramters for asset generator (5 to 10) arguments
+#- dividers on window horizontal and vertical
+#- 
+
 #Challenge features
-#User Interface
+#-
+
