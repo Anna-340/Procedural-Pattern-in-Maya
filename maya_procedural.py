50d1a63a53cf190e72b60e2951872d01a87d1a99
Annaliese <Annaliesepalm3@gmail.com>
2025-10-17 22:58:25 -0500

    planning pesuido code

--- maya_procedural.py
+++ maya_procedural.py
@@ -1,12 +1,18 @@
 #Three simple challenges or one complex one 
-#-
+#-trying to make the user interface more simplified and readable
+#-could add a slider for window contols like how big or small it is 
+#-color for flowers and possible shade for it
 
 #Custom Maya shelf button and icon & user interface
 
 #Number of intuative/paramters for asset generator (5 to 10) arguments
 #- dividers on window horizontal and vertical
-#- 
+#- flower count in front pot
+#- color change of pot
+#- possible curtain changes?
+#- window size in general
+#- change the colors of the flowers in the pot
 
 #Challenge features
-#-
+#- Extra research for UI interface
 
